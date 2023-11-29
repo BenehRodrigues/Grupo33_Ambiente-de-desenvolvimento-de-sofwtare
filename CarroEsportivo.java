1eb9757a86a8879781ef920b298d6676adda7c67
Ingrid <oligrid20@gmail.com>
2023-11-28 21:58:02 -0300

    CarroEsportivo

---
+++ CarroEsportivo.java
@@ -0,0 +1,18 @@
+public class CarroEsportivo extends Carro {
+ public CarroEsportivo(String marca, String modelo, int ano){
+  super( marca, modelo, ano);
+ }
+public void AbrirTurbina(){
+  System.out.println("Modo turbo ativado");
+
+ }
+ public void FecharTurbina(){
+  System.out.println("Modo turbo desativado");
+
+ }
+  @Override
+  public String toString() {
+    return super.toString();
+  }
+
+}
